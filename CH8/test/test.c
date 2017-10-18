f71e9673186b160ddc65d01dad30c37db3480188
renewment <renewmental@gmail.com>
2017-10-18 11:52:37 +0800

    20171018

--- CH8/test/test.c
+++
@@ -1,8 +0,0 @@
-//
-//  test.c
-//  
-//
-//  Created by 劉兆憲 on 2017/10/15.
-//
-
-#include <stdio.h>
--- CH8/twoNum.c
+++
@@ -1,8 +0,0 @@
-//
-//  twoNum.c
-//  
-//
-//  Created by 劉兆憲 on 2017/10/17.
-//
-
-#include <stdio.h>
